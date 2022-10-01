8d8b965f6e0a2ae25bbca8b30468df0c6c81cd13
Lucas Gaelzer Machado <lucasgaelzermachado@gmail.com>
2022-09-30 23:21:09 -0300

    Atualizacao 3

--- home.js
+++ home.js
@@ -24,14 +24,14 @@ function validacaoInputs () {
     const divErro = document.getElementsByClassName ("erro-mensagem")
 
     if (descricaoValue == "") {
-        erro ("Voce precisa preencher todos os campos!")
+        erro ("You need to fill in all the fields!")
         return
     } else {
         sucesso ()
     }
 
     if (detalhamentoValue == "") {
-        erro ("Voce precisa preencher todos os campos!")
+        erro ("You need to fill in all the fields!")
         return
     } else {
         sucesso ()
@@ -76,7 +76,7 @@ function adicionarTabela () {
                 <td>${recado.id}</td>
                 <td>${recado.descricao}</td>
                 <td>${recado.detalhamento}</td>
-                <td><button id="botao-editar" onclick="editarRecados(${recado.id})">Editar</button><button id="botao-excluir" onclick="removerRecados(${recado.id})">Excluir</button></td>
+                <td><button id="botao-editar" onclick="editarRecados(${recado.id})">Edit</button><button id="botao-excluir" onclick="removerRecados(${recado.id})">Delete</button></td>
             </tr>
         `
     }
@@ -95,7 +95,7 @@ function removerRecados (id) {
 
     salvarRecado (recados)
 
-    alert ("Recado removido com sucesso!")
+    alert ("Message successfully removed!")
 
     adicionarTabela ()
 }
@@ -105,25 +105,25 @@ function editarRecados (id) {
 
     const editRecados = recados.findIndex ((recado) => recado.id === id)
 
-    recados[editRecados].descricao = prompt ("Editar descrição: ")
+    recados[editRecados].descricao = prompt ("Edit Description: ")
 
-    recados[editRecados].detalhamento = prompt ("Editar detalhamento: ")
+    recados[editRecados].detalhamento = prompt ("Edit Detail: ")
 
     if (recados[editRecados].detalhamento == "" || recados[editRecados].detalhamento == null) {
-        alert ("Voce deve preencer os campos!")
+        alert ("You must fill in the fields!")
         return 
     } else {
         salvarRecado (recados)
     }
 
     if (recados[editRecados].descricao == "" || recados[editRecados].descricao == null) {
-        alert ("Voce deve preencer os campos!")
+        alert ("You must fill in the fields!")
         return
     } else {
         salvarRecado (recados)
     }
 
-    alert ("Recado editado com sucesso!")
+    alert ("Message edited successfully!")
 
     adicionarTabela()
 }
